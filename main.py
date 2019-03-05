f9a51f7064af18152cccb71c29ab996b7bd7e471
Jeremy Ronquillo <j_ronquillo@u.pacific.edu>
2019-03-05 12:54:53 -0800

    program now reads in directory for training/testing files, prints results

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ Disputed: 15 texts
 
 import sys
 import pdb
+import math
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
@@ -30,19 +31,42 @@ possible_words = 1000.0
 
 def main(argv, argc):
     if (argc < 3):
-        print("Usage: python main.py <training-set> <testing-set>")
+        print("Usage: python main.py <known-author-dir> <unknown-author-dir>")
         return 1
-    training_file, testing_file = file_IO(argv)
+    training_set, testing_set = file_IO(argv)
 
-    training_set = get_word_set(training_file)
-    testing_set = get_word_set(testing_file)
+    bayes_scores = {}
 
-    bayes_theorem_with_no_divisor(["to"], training_set, jay_prob)
+    print("Running Bayes Theorem with Hamilton Probability...")
+    bayes_scores["hamilton"] = bayes_theorem_with_no_divisor(testing_set, training_set, hamilton_prob)
+
+    print("Running Bayes Theorem with Jay Probability...")
+    bayes_scores["jay"] = bayes_theorem_with_no_divisor(testing_set, training_set, jay_prob)
+
+    print("Running Bayes Theorem with Madison Probability...")
+    bayes_scores["madison"] = bayes_theorem_with_no_divisor(testing_set, training_set, madison_prob)
+
+    print_results(bayes_scores)
 
     pdb.set_trace()
 
+def print_results(bayes_scores):
+    print("Bayes scores:")
+    for k in bayes_scores.keys():
+        print(k + ":     \t" + str(bayes_scores[k]))
 
-def get_word_set(file):
+    print("The author of the unknown classification is probably " + get_author_with_highest_prob(bayes_scores) + ".")
+
+
+def get_author_with_highest_prob(bayes_scores):
+    highest_prob = max([bayes_scores["hamilton"], bayes_scores["jay"], bayes_scores["madison"]])
+    for k in bayes_scores.keys():
+        if bayes_scores[k] == highest_prob:
+            return k
+    return None
+
+
+def preprocessing(file):
     contents = file_data_to_string(file)
     filtered_stop = remove_stop_words(contents)
     stem = stem_words(filtered_stop)
@@ -79,7 +103,7 @@ def remove_stop_words(contents):
 # author_prob: priori probability of text being the current author's
 def bayes_theorem_with_no_divisor(features, text, author_prob):
     text_length = float(len(text))
-    cond_prob = 1.0
+    cond_prob = 0.0
     smoothing_value = 1.0
     # find the conditional probability with each feature using bayes' theorem that applies Laplace smoothing
     # multiply each feature's probability with each other
@@ -90,29 +114,50 @@ def bayes_theorem_with_no_divisor(features, text, author_prob):
         numerator = float(feature_count + smoothing_value)
         denominator = float(text_length + possible_words)
 
-        cond_prob *= (numerator / denominator) * author_prob
-        
+        cond_prob += math.log((numerator / denominator) * author_prob)
+
     return cond_prob
 
 def file_IO(argv):
+    print("Performing file I/O...")
+
     params = {
-        "training_file": 1,
-        "testing_file": 2
+        "training_dir": 1,
+        "testing_dir": 2
     }
     training_file = None
     testing_file = None
-    try:
-        training_file = open(argv[params["training_file"]], "r")
-    except IOError:
-        print("Couldn't find file: " + str(argv[params["training_file"]]))
+
+    training_set = []
+    testing_set = []
+    files_not_found = 0
+    files_found_training = 0
+    files_found_testing = 0
+
+    for i in range(0, 100):
+        try:
+            training_filename = argv[params["training_dir"]] + str(i) + ".txt"
+            training_file = open(training_filename, "r")
+            training_set += preprocessing(training_file)
+            files_found_training += 1
+        except IOError:
+            files_not_found += 1
+        try:
+            testing_filename = argv[params["testing_dir"]] + str(i) + ".txt"
+            testing_file = open(testing_filename, "r")
+            testing_set += preprocessing(testing_file)
+            files_found_testing += 1
+        except IOError:
+            files_not_found += 1
+
+    if files_found_training == 0:
+        print("No files found for building the training set. Please check if the directory is correct and files range from 0.txt to 100.txt.")
         exit(1)
-    try:
-        testing_file = open(argv[params["testing_file"]], "r")
-    except IOError:
-        print("Couldn't find file: " + str(argv[params["testing_file"]]))
+    elif files_found_testing == 0:
+        print("No files found for building the testing set. Please check if the directory is correct and files range from 0.txt to 100.txt.")
         exit(1)
 
-    return training_file, testing_file
+    return training_set, testing_set
 
 
 def file_data_to_string(file):
