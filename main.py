266ceea5ee77b750fee85f35b29585277b16e8c8
JohnK0 <johnskim97@gmail.com>
2019-03-05 22:26:17 -0800

    possible_words works now and touched on function calls

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ jay_prob = 5.0/70.0
 madison_prob = 14.0/70.0
 
 # need to find the num of all the possible words (from all the texts in the Hamilton, Jay, Madison, and Disputed texts)
-possible_words = 1000.0
+possible_words = 0
 
 def main(argv, argc):
     if (argc != 5):
@@ -39,22 +39,22 @@ def main(argv, argc):
 
 #   training_set: [[hamilton set] [jay set] [madison set]]
 #   testing_set: [disputed set]
-    training_set, testing_set = file_IO(argv)
+    training_set = file_IO(argv)
 
     bayes_scores = {}
 
 #   features: list of features
-    features = input("What features do you want to use for determining disputed texts? (separate words and symbols by \"+\")")
+    features = raw_input("What features do you want to use for determining disputed texts? (separate words and symbols by \"+\")")
     features = features.split("+")
 
     print("Running Bayes Theorem with Hamilton Probability...")
-    bayes_scores["hamilton"] = bayes_theorem_with_no_divisor(features, testing_set, training_set, hamilton_prob)
+    bayes_scores["hamilton"] = bayes_theorem_with_no_divisor(features, training_set[0], hamilton_prob)
 
     print("Running Bayes Theorem with Jay Probability...")
-    bayes_scores["jay"] = bayes_theorem_with_no_divisor(features, testing_set, training_set, jay_prob)
+    bayes_scores["jay"] = bayes_theorem_with_no_divisor(features, training_set[1], jay_prob)
 
     print("Running Bayes Theorem with Madison Probability...")
-    bayes_scores["madison"] = bayes_theorem_with_no_divisor(features, testing_set, training_set, madison_prob)
+    bayes_scores["madison"] = bayes_theorem_with_no_divisor(features, training_set[2], madison_prob)
 
     print_results(bayes_scores)
 
@@ -139,19 +139,26 @@ def file_IO(argv):
     training_set = [[],[],[]]
     testing_set = []
     i = 0
-    pdb.set_trace()
+
     for directory in author_directories:
         for file in directory:
             text_file = open(file, "r")
             if i < len(author_directories)-1:
-                training_set[i].append(preprocessing(text_file))
+                training_set[i] += (preprocessing(text_file))
             else:
-                testing_set.append(preprocessing(text_file))
+                testing_set += (preprocessing(text_file))
         i += 1
 
-    pdb.set_trace()
+    distinct_words = list(set(testing_set))
+    for collection in training_set:
+        distinct_words += list(set(collection))
 
-    return training_set, testing_set
+    distinct_words = list(set(distinct_words))
+
+    global possible_words
+    possible_words += len(distinct_words)
+
+    return training_set
 '''
     params = {
         "training_dir": 1,
